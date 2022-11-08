7141e24e93cfdef68b5c80a1824e858d504de6a2
shahriar201 <shahriarbrine201@gmail.com>
2022-11-08 10:41:46 +0600

    login table change

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,8 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $table = 'employees';
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
---
+++ database/migrations/2022_11_07_134556_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->tinyInteger('status')->comment('1=active, 0=inactive')->default(1);
+            $table->string('password')->nullable();
+            $table->string('google_id')->nullable();
+            $table->tinyInteger('created_by')->nullable();
+            $table->tinyInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
